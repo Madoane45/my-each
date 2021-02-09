689d9a5e224d36c9d01e1b98a195c0172c61e050
Madoane45 <Madoane45@gmail.com>
2021-02-09 16:01:41 -0500

    commit tree 32020642ae86b6da567d6bec017d80413368d182

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+require"pry"
+def my_each(words)
+  i = 0
+  while words.length > i do 
+    yield (words[i])
+    i += 1
+  end
+  #my_each() ["hi", "hello", "bye", "goodbye"]
+ # binding.pry
+ #   return word  
+ # end
+ words
 end
+
+#my_each(words) do |word|
